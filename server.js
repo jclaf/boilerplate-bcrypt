935a358f6e2abe54f5a7dd7fe5b0da9b2bf24621
Jean-Christophe LAFLEUR <fabrice.972.20@gmail.com>
2025-06-24 23:39:51 +0200

    bcrypt done

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
 const bcrypt = require('bcrypt');
+
+
+// Ajoute une route pour la racine
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+  
+  // Démarre le serveur
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
 
