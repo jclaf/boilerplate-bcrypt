0973b3581e376a6c204b0eb6d74e92da6625ee79
Jean-Christophe LAFLEUR <fabrice.972.20@gmail.com>
2025-06-25 00:14:00 +0200

    bcrypt start

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 'use strict';
 const express     = require('express');
 const bodyParser  = require('body-parser');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
-const bcrypt = require('bcrypt');
+const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+const bcrypt      = require('bcrypt');
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
@@ -55,7 +55,7 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//     console.log("Listening on port:", PORT)
-// });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Listening on port:", PORT)
+});
