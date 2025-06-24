8c2f685415784e67d1460e8b0a2008e00d65c41c
Jean-Christophe LAFLEUR <fabrice.972.20@gmail.com>
2025-06-25 01:11:02 +0200

    bcrypt start

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 'use strict';
 const express     = require('express');
 const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const bcrypt = require('bcrypt');
+const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+
 const app         = express();
 
 
