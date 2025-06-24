5feac15496a9448578e2eb436e567631d535f298
Jean-Christophe LAFLEUR <fabrice.972.20@gmail.com>
2025-06-25 00:10:58 +0200

    bcrypt start

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log("Listening on port:", PORT)
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log("Listening on port:", PORT)
+// });
