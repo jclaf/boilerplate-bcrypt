6c802901742db9f95c9cd43b13ec19ece1ec856a
Jean-Christophe LAFLEUR <fabrice.972.20@gmail.com>
2025-06-24 23:56:06 +0200

    bcrypt start

--- server.js
+++ server.js
@@ -2,13 +2,15 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const bcrypt = require('bcrypt');
+
 const app         = express();
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-const bcrypt = require('bcrypt');
+
 
 
 // Ajoute une route pour la racine
