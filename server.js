be950ab0238f00587e2df7aff9d0abb73ffd9197
Jean-Christophe LAFLEUR <fabrice.972.20@gmail.com>
2025-06-25 17:39:59 +0200

    bcrypt done

--- server.js
+++ server.js
@@ -1,64 +1,35 @@
-'use strict';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const bcrypt = require('bcrypt');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+"use strict";
+const express = require("express");
+const bodyParser = require("body-parser");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const bcrypt = require("bcrypt");
+const cors = require('cors'); // Fix issue with CORS config on Glitch server.
 
-const app         = express();
 
+const app = express();
+app.use(cors());
+fccTesting(app); //For FCC testing purposes
 
-fccTesting(app);
 const saltRounds = 12;
-const myPlaintextPassword = 'sUperpassw0rd!';
-const someOtherPlaintextPassword = 'pass123';
-
-
-// Ajoute une route pour la racine
-// app.get('/', (req, res) => {
-//     res.send('Hello, world!');
-// });
+const myPlaintextPassword = "sUperpassw0rd!";
+const someOtherPlaintextPassword = "pass123";
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  /*Store hash in your db*/
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+  /*res == true or false*/
+    console.log(hash);
+  });
+});
 //END_ASYNC
 
 //START_SYNC
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 
 //END_SYNC
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log("Listening on port:", PORT)
-});
+app.listen(process.env.PORT || 3000, () => {});
